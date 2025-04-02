aafafe0584c0d88ed62416f7f2f9fa8e720a36ea
Sviatoslav Romankiv <romankiv1771@gmail.com>
2025-04-02 18:19:06 +0300

    Update diagnostics config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -69,10 +69,12 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Make diagnostics appear as virtual lines for better visibility
+vim.diagnostic.config({
+  virtual_lines = true
+})
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
